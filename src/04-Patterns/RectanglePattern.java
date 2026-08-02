433f417d1f2d574b82bf5275edb84784eb6b86ed
Naga-lakshmi10 <nagalakshmikarri27@gmail.com>
2026-08-02 19:58:42 +0530

    Add pattern printing programs

---
+++ src/04-Patterns/RectanglePattern.java
@@ -0,0 +1,10 @@
+public class RectanglePattern
+{
+    public static void main(String[] args)
+    {
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+    }
+}
