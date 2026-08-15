e215b7820a462bb71ffe83a02605f3b0709ac800
Naga-lakshmi10 <nagalakshmikarri27@gmail.com>
2026-08-15 18:52:46 +0530

    Add Escapecharacters.jav program

---
+++ src/01-Hello-World/EscapeCharacters.java
@@ -0,0 +1,19 @@
+public class EscapeCharacters
+{
+    public static void main(String[] args)
+    {
+        System.out.println("================================");
+        System.out.println("       Student Information     ");
+        System.out.println("================================\n");
+        System.out.println("Name:\tNagalakshmi");
+        System.out.println("Age:\t20");
+        System.out.println("Branch:\t\"Computer Science Engineering\"\n ");
+                System.out.println("File Path:");
+        System.out.println("c:\\users\\practice\\java");
+
+
+
+
+
+    }
+}
